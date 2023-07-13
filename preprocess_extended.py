d2d7a4ab8e81f405a8e80bae013dd3a17447e2e9
William Y. Feng <womogenes2@gmail.com>
2023-07-13 16:59:56 -0400

    Remove podcast episodes

--- preprocess_extended.py
+++ preprocess_extended.py
@@ -20,7 +20,7 @@ for file in sorted(os.listdir(folder_path)):
     with open(f"{folder_path}/{file}", "rb") as fin:
         data = json.load(fin)
         for stream in tqdm(data):
-            all_streams.append([
+            entry = [
                 dt.datetime.strftime(
                     dt.datetime.strptime(stream["ts"], "%Y-%m-%dT%H:%M:%SZ"),
                     "%Y-%m-%d %H:%M"
@@ -28,14 +28,25 @@ for file in sorted(os.listdir(folder_path)):
                 stream["master_metadata_album_artist_name"],
                 stream["master_metadata_track_name"],
                 stream["ms_played"]
-            ])
+            ]
+
+            if entry[1] == None or entry[2] == None:
+                # Probably a podcast episode
+                continue
+
+            all_streams.append(entry)
 
     print()
 
 with open("./data/all_streams.json", "wb") as fout:
     fout.write(b"[\n")
-    for stream in all_streams:
-        fout.write(("  " + json.dumps(stream) + ",\n").encode())
+    for i, stream in enumerate(all_streams):
+        line = "  " + json.dumps(stream)
+        if i < len(all_streams) - 1:
+            line += ","
+        line += "\n"
+        fout.write(line.encode())
+
     fout.write(b"]\n")
 
 print(f"Done.")
